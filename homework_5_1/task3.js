b0ebe86b0766b284ca88fd6e660559638be50cbf
Yuliia Obukhivska <90201962+YuliaObukhivska@users.noreply.github.com>
2026-02-17 23:31:07 +0100

    homework 5.1

---
+++ homework_5_1/task3.js
@@ -0,0 +1,6 @@
+const number = 3;
+for (let i = 1; i <= 10; i++) {
+    let multiply = number * i;
+    let result = number + ' x ' + i + ' = ' + multiply;
+    console.log(result);
+}
---
+++ homework_5_1/task3_1.js
@@ -0,0 +1,7 @@
+const number = 3;
+let count = 1;
+while (count <= 10) {
+    let result = number * count;
+    console.log(number + " x " + count + " = " + result); 
+    count++;
+}
