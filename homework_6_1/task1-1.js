5346369f4f2b7dcdb14b3569b3b10702b88b8c90
Yuliia-ob <60600725+Yuliia-ob@users.noreply.github.com>
2026-02-27 16:18:41 +0100

    homework-6-1 (#5)
    Co-authored-by: Yuliia Obukhivska <90201962+YuliaObukhivska@users.noreply.github.com>

---
+++ homework_6_1/task1-1.js
@@ -0,0 +1,5 @@
+function rectangleSquare(width, height) {
+  return width*height;
+}
+
+console.log(rectangleSquare(5,10));
---
+++ homework_6_1/task1-3.js
@@ -0,0 +1,5 @@
+const rectangleSquare = (width, height) => width*height;
+
+
+let a =rectangleSquare (5,10);
+console.log(a);
