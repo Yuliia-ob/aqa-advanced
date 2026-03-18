ed0637e2b59bae52feac2f0081f03547299dc679
Yuliia Obukhivska <90201962+YuliaObukhivska@users.noreply.github.com>
2026-03-18 23:10:47 +0100

    homework_9_1

---
+++ homework_9_1/task1.js
@@ -0,0 +1,7 @@
+const student = {
+  firstName: "Yuliia",
+  lastName: "Obukhivska",
+  age: 32,
+  courses: ["Математика", "Історія", "Програмування"]
+};
+console.log(student);
---
+++ homework_9_1/task2.js
@@ -0,0 +1,10 @@
+const book = {
+  title: "The Little Prince",
+  author: "Antoine de Saint-Exupéry",
+  year: 1943
+};
+const { title, author, year } = book;
+
+console.log(title); 
+console.log(author); 
+console.log(year); 
